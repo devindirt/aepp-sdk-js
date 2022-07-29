b0a2a80f2da78ab574d6eae38fa863d3f640b245
Jeevanjot Singh Vital <60998339+devindirt@users.noreply.github.com>
2022-07-29 16:59:14 +0100

    Update chain.ts

--- src/chain.ts
+++ src/chain.ts
@@ -89,7 +89,7 @@ export async function getHeight({ onNode }: { onNode: Node }): Promise<number> {
 export async function poll(
   th: Encoded.TxHash,
   {
-    blocks = 10, interval, onNode, ...options
+    blocks = 20000, interval, onNode, ...options
   }:
   { blocks?: number; interval?: number; onNode: Node } & Parameters<typeof _getPollInterval>[1],
 ): Promise<TransformNodeType<SignedTx>> {
